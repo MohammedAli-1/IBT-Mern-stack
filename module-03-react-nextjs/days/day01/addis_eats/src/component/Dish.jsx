5043928e4f221f95c36783d39b435246420f886b
unknown <https://github.com/MohammedAli-1>
2026-09-02 21:45:25 +0300

    Learn React PropTypes

--- module-03-react-nextjs/days/day01/addis_eats/src/component/Dish.jsx
+++ module-03-react-nextjs/days/day01/addis_eats/src/component/Dish.jsx
@@ -1,10 +1,32 @@
-function Dish({ name, price }) {
+import Card from "./Card";
+import { useState } from "react";
+import { PropTypes } from "prop-types";
+function Dish({ name, price, isSlice, category, currency = "ETB", onAdd }) {
+  // const [count, setCount] = useState(0);
+  // function Add() {
+  //   return setCount(count + 1);
+  // }
   return (
     <div className="card">
-      <h1>{name}</h1>
-      <p>{price}</p>
+      <Card>
+        <h1>{name}</h1>
+        <p>
+          {price} {currency}
+        </p>
+        <p>{category}</p>
+        <em>{isSlice && "Slice"}</em>
+        <button onClick={() => onAdd(price)}>Add</button>
+        {/* <p>Quantity:{count}</p> */}
+      </Card>
     </div>
   );
 }
+Dish.propTypes = {
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  isSlice: PropTypes.bool,
+  category: PropTypes.string.isRequired,
+  currency: PropTypes.string,
+};
 
 export default Dish;
--- module-03-react-nextjs/days/day01/addis_eats/src/component/Main.jsx
+++ module-03-react-nextjs/days/day01/addis_eats/src/component/Main.jsx
@@ -1,16 +1,102 @@
 import Dish from "./Dish";
 function Main() {
   const menu = [
-    { id: 1, name: "Doro Wat", price: 240 },
-    { id: 2, name: "Shiro", price: 120 },
-    { id: 3, name: "Tibs", price: 280 },
+    // 🍗 Chicken
+    {
+      name: "Chicken Tikka",
+      price: 12.99,
+      id: 1,
+      isSlice: true,
+      category: "chicken",
+    },
+    {
+      name: "Chicken Burger",
+      price: 10.99,
+      id: 2,
+      isSlice: false,
+      category: "chicken",
+    },
+    {
+      name: "Grilled Chicken",
+      price: 13.99,
+      id: 3,
+      isSlice: false,
+      category: "chicken",
+    },
+
+    // 🥩 Beef
+    {
+      name: "Beef Burger",
+      price: 9.99,
+      id: 4,
+      isSlice: false,
+      category: "beef",
+    },
+    {
+      name: "Beef Steak",
+      price: 15.99,
+      id: 5,
+      isSlice: false,
+      category: "beef",
+    },
+    {
+      name: "Beef Pizza",
+      price: 12.99,
+      id: 6,
+      isSlice: true,
+      category: "beef",
+    },
+
+    // 🥗 Vegetarian
+    {
+      name: "Veggie Pizza",
+      price: 11.99,
+      id: 7,
+      isSlice: true,
+      category: "vegetarian",
+    },
+    {
+      name: "Vegetable Pasta",
+      price: 10.99,
+      id: 8,
+      isSlice: false,
+      category: "vegetarian",
+    },
+    {
+      name: "Garden Salad",
+      price: 8.99,
+      id: 9,
+      isSlice: false,
+      category: "vegetarian",
+    },
   ];
+  const chicken = menu.filter((item) => item.category == "chicken");
+  const vegetarian = menu.filter((item) => item.category == "vegetarian");
+  const beef = menu.filter((item) => item.category == "beef");
+  function addToOrder(price){
+    console.log(price)
+  }
   return (
-    <div >
-      {menu.map((item) => {
-        return <Dish key={item.id} name={item.name} price={item.price} />;
-      })}
-    </div>
+    <>
+      <h2>Chicken Types</h2>
+      <div className="dish">
+        {chicken.map((item) => {
+          return <Dish key={item.id} {...item} onAdd={addToOrder} />;
+        })}
+      </div>
+      <h2>vegetarian Types</h2>
+      <div className="dish">
+        {vegetarian.map((item) => {
+          return <Dish key={item.id} {...item} onAdd={addToOrder} />;
+        })}
+      </div>
+      <h2>Beef Types</h2>
+      <div className="dish">
+        {beef.map((item) => {
+          return <Dish key={item.id} {...item} onAdd={addToOrder}/>;
+        })}
+      </div>
+    </>
   );
 }
 
